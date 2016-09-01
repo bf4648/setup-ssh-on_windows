38492ebfb2cc813b6a0c3986886d735837f76e17
bf4648 <benjamin.frazier@gmail.com>
2016-08-31 22:23:46 -0400

    Update setupSSHOnWindows.sh

--- setupSSHOnWindows.sh
+++ setupSSHOnWindows.sh
@@ -2,34 +2,35 @@
 set -x
 #set -e
 
-#Run this script in cygwin using 'sh setupSSHOnWindows.sh' from the terminal
+#Run this script in cygwin using './setupSSHOnWindows.sh' from the terminal
 
-# References:  
+# Refs:  
 # 1.  	http://www.security-plus.co/OpenSSH.txt 
 # 2.  	http://www.noah.org/ssh/cygwin-sshd.html
 # 3.  	https://bscb.cornell.edu/about/resources/windows-installing-ssh-server
 # 4. 	https://github.com/vicrucann/rshell-mat
 # 5. 	http://superuser.com/questions/110726/how-to-uninstall-reinstall-cygwin-to-use-sshd
+# 6. 	techtorials.me/cygwin/sshd-configuration/	
 
 #You may not really need this script now...just run https://github.com/tschutter/AppData
-
 #Preforming the above will automatically set up openssh 
-#1. Admin --> cygrunsrv 
-#2. Net --> openssh
-#3.  Gawk
+#1.	Admin --> cygrunsrv 
+#2.	Net --> openssh
+#3.	Gawk
 #Optional
-#   Editors --> vim
-#   Utilities --> ncurses
-#   Net --> curl
-#   Net --> wget
-#   Net --> rsync
-
+#	Editors --> vim
+#	Utilities --> ncurses
+#	Net --> curl
+#	Net --> wget
+#	Net --> rsync
 #   *** ---Be sure to run Cygwin as Administrator --- ***
+
 FIREWALL_SERVICE_NAME="SSHD"
 CYGWIN_PATH="c:\cygwin64\usr\sbin\sshd.exe"
 FIREWALL_PROFILE="any"
 
-#TODO: go through the following tutorial and fix my code
+PASSWORD="9d4iPuFucyQ9dT4w"
+SSH_PASSPHRASE="test"
 # techtorials.me/cygwin/sshd-configuration/	
 
 #Remove Commands
@@ -62,38 +63,18 @@ remove_firewall_exception() {
 }
 
 #Set up Commands
-start_sshd_service_in_windows() {
-	#or net start sshd
-	net start sshd
-}
-
-setup_local_security_authority() {
-	echo "yes" | cyglsa-config
-}
-
-ssh_host_config() {
-	ssh-host-config -y
-}
-
-add_firewall_exception() {
-	#Add the firewall expection
-	netsh advfirewall firewall add rule name=""$FIREWALL_SERVICE_NAME"" dir=in action=allow program=""$CYGWIN_PATH"" profile=""$FIREWALL_PROFILE"" "$FIREWALL_SERVICE_NAME" enable=yes
-}
-
 fix_perms_on_passwd() {
-	
 	chmod +r /etc/passwd
 	chmod u+w /etc/passwd
 }
 
 fix_perms_on_group(){
-
 	chmod +r /etc/group
 	chmod u+w /etc/group
 }
 
-fix perms_on_var() {
-	chmod 755 /var
+fix_perms_on_var_file() {
+   	chmod 777 /var
 }
 
 create_sshd_log_file() {
@@ -109,14 +90,26 @@ edit_sshd_rights_on_windows() {
 	editrights -l -u sshd
 }
 
-fix_passwd() {
-	#In order to harmonize Windows user information with Cygwin
-	mkpasswd --local > /etc/passwd
+ssh_host_config() {
+	ssh-host-config --yes --privileged --user cyg_server --pwd "$PASSWORD"
 }
 
-fix_groups() {
-	#In order to harmonize Windows user information with Cygwin
-	mkgroup --local > /etc/group
+ssh_user_config() {
+	ssh-user-config --yes --word "$SSH_PASSPHRASE" 
+}
+
+setup_local_security_authority() {
+	echo "yes" | cyglsa-config
+}
+
+add_firewall_exception() {
+	#Add the firewall expection
+	netsh advfirewall firewall add rule name=""$FIREWALL_SERVICE_NAME"" dir=in action=allow program=""$CYGWIN_PATH"" profile=""$FIREWALL_PROFILE"" "$FIREWALL_SERVICE_NAME" enable=yes
+}
+
+start_sshd_service_in_windows() {
+	#or net start sshd
+	net start sshd
 }
 
 remove_ssh_on_windows() {
@@ -131,9 +124,19 @@ remove_ssh_on_windows() {
 }
 
 setup_ssh_on_windows() {
+
+	#fix perms
+	fix_perms_on_passwd
+	fix_perms_on_group
+	fix_perms_on_var_file
+
+
+	#create log file
+	create_sshd_log_file
+
+	#edit sshd rights
+	edit_sshd_rights_on_windows
 	ssh_host_config
-	fix_passwd
-	fix_groups
 	setup_local_security_authority
 	add_firewall_exception
 	start_sshd_service_in_windows
